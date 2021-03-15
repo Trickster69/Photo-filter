ad7a127ccb7e4e508536c07322a7259cc83345bd
Sergei Demchuk <vinci.ru@mail.ru>
2021-03-15 18:13:46 +0300

    feat:add btns animation

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -2,15 +2,18 @@ const controllers = document.querySelectorAll('.filters label'),
     img = document.querySelector('.editor img'),
     result = document.querySelectorAll('output'),
     resetBtn = document.querySelector('.btn-reset'),
-    saveBtn = document.querySelector('.btn-save'),
+    saveBtn = document.querySelector('.btn-save'),    
     rages = document.querySelectorAll('.filters input'),
     nextPicBtn = document.querySelector('.btn-next'),
     openPicBtn = document.querySelector('.btn-load--input'),
+    loadPicBtn = document.querySelector('.btn-load'),
+    allBtn = document.querySelectorAll('.btn'),
+    fullScreenBtn = document.querySelector('.fullscreen '),
     canvas = document.querySelector('canvas');
 let b = 1;   
 
 const imgC = new Image();
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 imgC.setAttribute('crossOrigin', 'anonymous'); 
 imgC.src = img.src;
 
@@ -21,74 +24,86 @@ imgC.onload = function() {
     ctx.drawImage(imgC, 0, 0);
   };
 
+function setActiveBtn(e){
+    allBtn.forEach(key=>{
+        key.classList.remove('btn-active')
+    });
+    e.currentTarget.classList.add('btn-active');
+}
+
   
 function change(e){
-    let arr2 = [];
+    let arrChanges = [];
     controllers.forEach(key => {
         let name = key.querySelector('input').name;
         let value = key.querySelector('input').value;
         let size = key.querySelector('input').dataset.sizing;
         key.querySelector('output').value = value;
         
-        arr2.push(`${name}(${value}${size})`);
-        ctx.filter = arr2.join("");
+        arrChanges.push(`${name}(${value}${size})`);
+        ctx.filter = arrChanges.join('');
         ctx.drawImage(imgC, 0, 0);
     });
+    
 }
 
 function reset(e){
-    let arr3 = [];    
+    let arrReset = [];    
 
     controllers.forEach(key => {
-        if(key.querySelector('input').name == "saturate"){            
+        if(key.querySelector('input').name == 'saturate'){            
             key.querySelector('input').value = 100;
             key.querySelector('output').value = 100;
             
-            arr3.push(`${key.querySelector('input').name}(${key.querySelector('input').value}${key.querySelector('input').dataset.sizing})`);  
-            ctx.filter = arr3.join("");          
+            arrReset.push(`${key.querySelector('input').name}(${key.querySelector('input').value}${key.querySelector('input').dataset.sizing})`);  
+            ctx.filter = arrReset.join('');          
             ctx.drawImage(imgC, 0, 0);
         }else{
             key.querySelector('input').value = 0;
             key.querySelector('output').value = 0;
 
-            arr3.push(`${key.querySelector('input').name}(${key.querySelector('input').value}${key.querySelector('input').dataset.sizing})`);            
-            ctx.filter = arr3.join("");
+            arrReset.push(`${key.querySelector('input').name}(${key.querySelector('input').value}${key.querySelector('input').dataset.sizing})`);            
+            ctx.filter = arrReset.join('');
             ctx.drawImage(imgC, 0, 0);
         }
     });
+    setActiveBtn(event);
 }
 
 
-controllers.forEach(key => key.addEventListener("input", ()=>{
+controllers.forEach(key => key.addEventListener('input', ()=>{
     change(event);
 }));
 resetBtn.addEventListener('click', ()=>{
     reset(event);
+    setActiveBtn(event);
 });
 saveBtn.addEventListener('click', ()=>{
+    setActiveBtn(event);  
     var link = document.createElement('a');
     link.download = 'download.png';
-    link.href = canvas.toDataURL("image/jpeg");
-    link.click();
-    // link.delete;
+    link.href = canvas.toDataURL('image/jpeg');
+    link.click();      
 });
+
 
 
 nextPicBtn.addEventListener('click', ()=>{
+    setActiveBtn(event);
     let now = new Date();
-    let timeDay = "";
+    let timeDay = '';
     let hours = now.getHours();
     let minutes = now.getMinutes();
     console.log(hours);
     
     if((hours >= 6 && minutes >=0)&& (hours <=11 && minutes <= 59)){
-        timeDay = "morning";
+        timeDay = 'morning';
     }else if((hours >= 12 && minutes >=0)&& (hours <=17 && minutes <= 59)){
-        timeDay = "day";
+        timeDay = 'day';
     }else if((hours >= 18 && minutes >=0)&& (hours <=23 && minutes <= 59)){
-        timeDay = "evening";
+        timeDay = 'evening';
     }else if((hours >= 0 && minutes >=0)&& (hours <=5 && minutes <= 59)){
-        timeDay = "night";
+        timeDay = 'night';
     }
     console.log(timeDay);
     imgC.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeDay}/${b <= 9?`0${b}`:b}.jpg`;
@@ -108,11 +123,17 @@ openPicBtn.addEventListener('change', (e)=> {
     reader.onload = () => {
         const img = new Image();
         imgC.src = reader.result;
-        imgC.src.innerHTML = "";
+        imgC.src.innerHTML = '';
         imgC.src.append(img);
 
-    }
+    };
     reader.readAsDataURL(file);
+});
+loadPicBtn.addEventListener('click', ()=>{
+    setActiveBtn(event);
 })
 
+fullScreenBtn.addEventListener('click',()=>{
+    
+})
 
